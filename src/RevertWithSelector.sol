dbcca5ca02d9be70b0ec73951d4af06362c86185
lumoswiz <lumoswiz@protonmail.com>
2025-09-01 11:53:50 +0200

    feat: solve RevertWithSelector

--- src/RevertWithSelector.sol
+++ src/RevertWithSelector.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+/**
+ *  No padding, so function selector does not start at index 0 (starts at index 28 (0x1c))
+ */
+
 contract RevertWithSelector {
     error RevertData(); // selector: 0xa3b7e096
 
     function main() external pure {
         assembly {
-            // your code here
+            mstore(0x00, 0xa3b7e096)
+            revert(0x1c, 0x04)
             // revert with the custom error "RevertData"
             // do the Solidity equivalent of
             // `revert RevertData()`
