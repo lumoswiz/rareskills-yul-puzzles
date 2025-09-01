dda3af66375a9ed6727230528a0bdee89b4f05cd
lumoswiz <lumoswiz@protonmail.com>
2025-09-01 13:17:19 +0200

    feat: solve RevertWithPanic

--- src/RevertWithPanic.sol
+++ src/RevertWithPanic.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.13;
 contract RevertWithPanic {
     function main() external pure {
         assembly {
-            // your code here
+            mstore(0x00, shl(224, 0x4e487b71))
+            mstore(0x04, 0x01)
+            revert(0x00, 0x24)
             // revert the function with an error of type `Panic(uint256)`
             // use "0x01" as error code
             // Hint: The error type is built-in and cannot be re-defined. See https://www.rareskills.io/post/try-catch-solidity
