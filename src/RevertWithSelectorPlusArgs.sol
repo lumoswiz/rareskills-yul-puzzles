5685181550c53b3ebc7f08cf21719008a43d38f4
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-01 13:12:39 +0200

    feat(puzzle-05): solve RevertWithSelectorPlusArgs (#6)
    feat: solve RevertWithSelectorPlusArgs

--- src/RevertWithSelectorPlusArgs.sol
+++ src/RevertWithSelectorPlusArgs.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+/**
+ * Remember: mstore overwrites
+ */
+
 contract RevertWithSelectorPlusArgs {
     error RevertData(uint256); // selector: 0xae412287
 
     function main(uint256 x) external pure {
         assembly {
-            // your code here
+            mstore(0x00, shl(224, 0xae412287))
+            mstore(0x04, x)
+            revert(0x00, 0x24)
             // revert custom error with x parameter
             // Hint: concatenate selector and x by storing them
             // adjacent to each other in memory
