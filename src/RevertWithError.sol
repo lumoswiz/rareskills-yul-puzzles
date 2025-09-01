0ebb644a0226e3f56096e5d5e6f4e2b3a79d6e68
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-01 13:00:40 +0200

    feat(puzzle-04): solver RevertWithError (#5)
    feat: solver RevertWithError

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.13;
 
 contract RevertWithError {
     function main() external pure {
+        bytes memory e = bytes("RevertRevert");
         assembly {
+            mstore(0x00, shl(0xE0, 0x08c379a0))
+            mstore(0x04, 0x20)
+            mstore(0x24, mload(e))
+            mstore(0x44, mload(add(e, 0x20)))
+            revert(0x00, 0x64)
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
