cd8f88fd3255410183b7c3a958551fbcc4e887ca
lumoswiz <lumoswiz@protonmail.com>
2025-09-02 15:36:14 +0200

    feat: solve KeccakX

--- src/KeccakX.sol
+++ src/KeccakX.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.13;
 contract KeccakX {
     function main(uint256 x) external pure returns (bytes32) {
         assembly {
-            // your code here
+            mstore(0x00, x)
+            mstore(0x00, keccak256(0x00, 0x20)) // overwriting x in memory
+            return(0x00, 0x20)
             // return the keccak hash of x
             // Hint: use keccak256(offset, size)
             // Hint: you need to put x in memory first
