339bb5205e82954308dc3682c2412fba68d5f10b
lumoswiz <lumoswiz@protonmail.com>
2025-09-03 14:09:52 +0200

    feat: solve WriteToDynamicArray

--- src/WriteToDynamicArray.sol
+++ src/WriteToDynamicArray.sol
@@ -6,7 +6,20 @@ contract WriteToDynamicArray {
 
     function main(uint256[] memory x) external {
         assembly {
-            // your code here
+            let h := 0x04
+            let o := calldataload(h)
+            let d := add(h, o)
+            let l := calldataload(d)
+            let p := add(d, 0x20)
+
+            let s := keccak256(0x00, 0x20)
+            // prettier-ignore
+            for { let i := 0} lt(i, l) { i := add(i, 0x01)} {   
+                sstore(s, calldataload(p))
+                s := add(s, 0x01)
+                p := add(p, 0x20)
+            }
+            sstore(0x00, l)
             // store the values in the DYNAMIC array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
         }
