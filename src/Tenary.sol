00e8290ba5b6d39b072e85d4df8967028209a6d9
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-03 13:29:08 +0200

    feat(puzzle-21): solve Tenary (#22)
    feat: solve Tenary

--- src/Tenary.sol
+++ src/Tenary.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+/**
+ * How it works:
+ *
+ * Let d = xor(x, y) & f = iszero(condition), g = mul(d, f)
+ *
+ * condition = 0 => f = 1 => g => d, then:
+ * z = x ^ d = x ^ (x ^ y) = y
+ *
+ * condition = 1 => f = 0 => g = 0
+ * z = x ^ 0 = x
+ */
+
 contract Tenary {
     uint256 public result;
 
     function main(uint256 a, uint256 b, uint256 c) external {
         assembly {
-            // your code here
+            let z := xor(
+                0x0a,
+                mul(
+                    xor(
+                        0x0a,
+                        xor(0x14, mul(xor(0x14, 0x1E), iszero(gt(b, c))))
+                    ),
+                    iszero(gt(a, b))
+                )
+            )
+            sstore(0x00, z)
             // store `10` in the storage variable `result` if a > b,
             // else if b > c, store `20`,
             // else, store `30`
