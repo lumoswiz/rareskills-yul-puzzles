8d21e155aaa734af5ffeaf2aca4035754f4aed06
lumoswiz <lumoswiz@protonmail.com>
2025-09-03 20:35:55 +0200

    feat: solve WriteToPacked128

--- src/WriteToPacked128.sol
+++ src/WriteToPacked128.sol
@@ -1,13 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+/**
+ * How it works:
+ *   - mask used to ensure cleanliness (i.e. don't overwrite `someValue`)
+ *   - x preserves upper 128 bits only
+ *   - or(...): we get both x and cleaned v allocated
+ */
+
 contract WriteToPacked128 {
     uint128 public writeHere = 1;
     uint128 public someValue = 7;
 
     function main(uint256 v) external {
         assembly {
-            // your code here
+            let mask := sub(shl(128, 1), 1)
+            let x := shl(128, shr(128, sload(0)))
+            sstore(0, or(x, and(v, mask)))
+
             // change the value of `writeHere` storage variable to `v`
             // be careful not to alter the value of `someValue` variable
             // Hint: storage slots are arranged sequentially. Determine the storage slot of `writeHere`
