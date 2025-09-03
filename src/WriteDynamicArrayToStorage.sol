e52c9848840f0d99a16fda0cdf5282fa8263c314
lumoswiz <lumoswiz@protonmail.com>
2025-09-03 17:44:40 +0200

    feat: solve WriteDynamicArrayToStorage

--- src/WriteDynamicArrayToStorage.sol
+++ src/WriteDynamicArrayToStorage.sol
@@ -6,7 +6,20 @@ contract WriteDynamicArrayToStorage {
 
     function main(uint256[] calldata x) external {
         assembly {
-            // your code here
+            let ptr := keccak256(0x00, 0x20)
+
+            calldatacopy(0x00, 0x04, sub(calldatasize(), 0x04))
+            let off := mload(0x00)
+            let len := mload(off)
+            sstore(0x00, len)
+
+            let data := add(off, 0x20)
+            // prettier-ignore
+            for {let i := 0} lt(i, len) {i := add(i, 1)} {
+                sstore(ptr, mload(data))
+                ptr := add(ptr, 1)
+                data := add(data, 0x20)
+            }
             // write the dynamic calldata array `x` to storage variable `writeHere`
         }
     }
