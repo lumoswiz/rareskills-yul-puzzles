e1e5bc4160dc7d19b45aebdbabd9eec5f7d2a1e6
lumoswiz <lumoswiz@protonmail.com>
2025-09-03 16:46:58 +0200

    feat: solver WriteToDoubleMapping

--- src/WriteToDoubleMapping.sol
+++ src/WriteToDoubleMapping.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract WriteToDoubleMapping {
-    mapping(address user => mapping(address token => uint256 value)) public balances;
+    mapping(address user => mapping(address token => uint256 value))
+        public balances;
 
     function main(address user, address token, uint256 value) external {
         assembly {
-            // your code here
+            mstore(0x00, user)
+            let innerHash := keccak256(0x00, 0x40)
+            mstore(0x20, innerHash)
+            mstore(0x00, token)
+            let s := keccak256(0x00, 0x40)
+            sstore(s, value)
             // set the `value` for a `user` and a `token`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
         }
