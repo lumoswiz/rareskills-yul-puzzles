1d3d59ea79e6d6aadac9e4defa2055d12a41c0d1
lumoswiz <lumoswiz@protonmail.com>
2025-09-04 20:15:29 +0200

    feat: solve ReturnArrayOfUint256

--- src/ReturnArrayOfUint256.sol
+++ src/ReturnArrayOfUint256.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.13;
 
 contract ReturnArrayOfUint256 {
-    function main(uint256 a, uint256 b, uint256 c) external pure returns (uint256[] memory) {
+    function main(
+        uint256 a,
+        uint256 b,
+        uint256 c
+    ) external pure returns (uint256[] memory) {
         assembly {
-            // your code here
+            let p := mload(0x40)
+            mstore(p, 0x20)
+            mstore(add(p, 0x20), 0x03)
+            mstore(add(p, 0x40), a)
+            mstore(add(p, 0x60), b)
+            mstore(add(p, 0x80), c)
+            return(p, add(p, 0xa0))
             // return an array of [a,b,c]
         }
     }
