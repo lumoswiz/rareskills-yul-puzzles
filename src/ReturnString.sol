f83a5fb3f9f19d122365af2dd1fb74641db37dee
lumoswiz <lumoswiz@protonmail.com>
2025-09-04 20:20:44 +0200

    feat: solve ReturnString

--- src/ReturnString.sol
+++ src/ReturnString.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.13;
 contract ReturnString {
     function main() external pure returns (string memory) {
         assembly {
-            // your code here
+            let p := mload(0x40)
+
+            mstore(p, 0x20)
+            mstore(add(p, 0x20), 17)
+            mstore(
+                add(p, 0x40),
+                0x48656c6c6f2c2052617265536b696c6c73000000000000000000000000000000
+            )
+            return(p, 0x60)
             // return the exact string: `Hello, RareSkills`
         }
     }
