665daa7974ce02dfbb69b3db962194794b6e1728
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-04 20:31:34 +0200

    feat: solve ReturnBytes (#56)

--- src/ReturnBytes.sol
+++ src/ReturnBytes.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.13;
 contract ReturnBytes {
     function main(address a, uint256 b) external pure returns (bytes memory) {
         assembly {
-            // your code here
+            let p := mload(0x40)
+            mstore(p, 0x20)
+            mstore(add(p, 0x20), 0x40)
+            mstore(add(p, 0x40), a)
+            mstore(add(p, 0x60), b)
+            mstore(0x40, add(p, 0x80))
+            return(p, 0x80)
             // encode a and b `abi.encode(a,b)` and return it.
         }
     }
