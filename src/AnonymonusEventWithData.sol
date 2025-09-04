a7dd5b0e0b3dd29fea404f0fa6462e8220df95b7
lumoswiz <lumoswiz@protonmail.com>
2025-09-04 11:19:46 +0200

    feat: solve AnonymonusEventWithData

--- src/AnonymonusEventWithData.sol
+++ src/AnonymonusEventWithData.sol
@@ -7,7 +7,8 @@ contract AnonymonusEventWithData {
 
     function main(uint256 num) external {
         assembly {
-            // your code here
+            mstore(0x00, num)
+            log0(0x00, 0x20)
             // emit the `MyEvent(uint256)` without using the event hash as a topic.
             // Anonymous events don't have the event signature (topic0) included.
             // Hint: Use log0 to emit the event with only the data.
