9f5996c35fa007d6e4e7f8a764b271679c8f0af2
lumoswiz <lumoswiz@protonmail.com>
2025-09-04 11:23:48 +0200

    feat: solve AnonymonusEventWithIndexedData

--- src/AnonymonusEventWithIndexedData.sol
+++ src/AnonymonusEventWithIndexedData.sol
@@ -7,7 +7,8 @@ contract AnonymonusEventWithIndexedData {
 
     function main(address emitter, bytes32 id, uint256 num) external {
         assembly {
-            // your code here
+            mstore(0x00, num)
+            log3(0x00, 0x20, 0x00, emitter, id)
             // emit the `MyEvent(address,bytes32,uint256)` event.
             // Hint: Use `log3` to emit the event with three parameters, without including the event signature (topic0).
             // Since this is an anonymous event, it does not include the event hash (topic0).
