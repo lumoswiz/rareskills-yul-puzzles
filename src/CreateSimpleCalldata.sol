0ac40df09a61ab36abfce5940d2166674f0a5f89
lumoswiz <lumoswiz@protonmail.com>
2025-09-05 14:22:51 +0200

    feat: solve CreateSimpleCalldata

--- src/CreateSimpleCalldata.sol
+++ src/CreateSimpleCalldata.sol
@@ -2,13 +2,27 @@
 pragma solidity ^0.8.13;
 
 contract CreateSimpleCalldata {
-    function main(bytes calldata deploymentBytecode) external returns (address) {
+    function main(
+        bytes calldata deploymentBytecode
+    ) external returns (address) {
         assembly {
-            // your code here
+            let off := calldataload(0x04)
+            let ptr := add(0x04, off)
+            let len := calldataload(ptr)
+            let dbc := add(ptr, 0x20)
+
+            let p := mload(0x40)
+            calldatacopy(p, dbc, len)
+
+            let addr := create(0, p, len)
+
+            mstore(0x00, addr)
+            return(0x00, 0x20)
+
             // create a contract using the deploymentBytecode
             // return the address of the contract
             // hint: use the `create` opcode
             // hint: use calldatacopy to copy the deploymentBytecode to memory
-       }
+        }
     }
 }
