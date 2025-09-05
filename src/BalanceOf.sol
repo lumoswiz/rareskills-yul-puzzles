d740b6e09a82340985fbf708d89d6714f668fbe7
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-05 13:58:13 +0200

    feat: solve BalanceOf (#71)

--- src/BalanceOf.sol
+++ src/BalanceOf.sol
@@ -5,13 +5,17 @@ contract BalanceOf {
     // emit these
     function main(address token) external view returns (uint256) {
         assembly {
-            // your code here
+            mstore(0x00, 0x70a08231)
+            mstore(0x20, address())
+            let success := staticcall(gas(), token, 0x1c, 0x40, 0x00, 0x00)
+            let p := mload(0x40)
+            returndatacopy(p, 0x00, 0x20)
+            return(p, 0x20)
             // return the balance of this contract using ERC20 "token"
             // assume "token" is an ERC20 contract
             // hint: balanceOf has function selector 0x70a08231 and signature "balanceOf(address)"
             // hint: since this is a view function, you should use staticcall
             // hint: use `returndatacopy` to copy the return data to the memory
-       }
+        }
     }
 }
-
