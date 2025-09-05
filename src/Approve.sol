c1d911ea35a2f8c74ec8d754c9ba29a74de82bb8
lumoswiz <lumoswiz@protonmail.com>
2025-09-05 11:37:06 +0200

    feat: solve Approve

--- src/Approve.sol
+++ src/Approve.sol
@@ -5,10 +5,21 @@ contract Approve {
     // emit these
     function main(address token, uint256 amount) external {
         assembly {
-            // your code here
+            let p := mload(0x40)
+            mstore(p, 0x095ea7b3)
+            mstore(add(p, 0x20), token)
+            mstore(add(p, 0x40), amount)
+            let success := call(
+                gas(),
+                token,
+                0x00,
+                add(p, 0x1c),
+                0x44,
+                0x00,
+                0x00
+            )
             // approve "token" to spend "amount"
             // hint: approve has function selector 0x095ea7b3 and signature "approve(address,uint256)"
-       }
+        }
     }
 }
-
