c7ab942bac28b47075447b3fcf451ce7de3f7dbc
lumoswiz <lumoswiz@protonmail.com>
2025-09-05 11:20:06 +0200

    feat: solve SimpleCall

--- src/SimpleCall.sol
+++ src/SimpleCall.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.13;
 
 contract SimpleCall {
-
     function main(address t) external payable {
         assembly {
-            // your code here
+            mstore(0x40, 0xc2985578)
+            let success := call(gas(), t, callvalue(), 0x5c, 0x04, 0xe0, 0x00)
             // call "t.foo()"
             // hint: "foo()" has function selector 0xc2985578
-       }
+        }
     }
 }
