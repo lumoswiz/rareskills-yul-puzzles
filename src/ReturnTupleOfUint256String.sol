0537449fbc2a98939e2f7f219f298353ece533bd
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-05 09:40:53 +0200

    feat: solve ReturnTupleOfUint256String (#60)

--- src/ReturnTupleOfUint256String.sol
+++ src/ReturnTupleOfUint256String.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.13;
 contract ReturnTupleOfUint256String {
     function main() external pure returns (uint256, string memory) {
         assembly {
-            // your code here
+            let p := mload(0x40)
+
+            mstore(p, 0x1A4)
+            mstore(add(p, 0x20), 0x40)
+            mstore(add(p, 0x40), 0x0a)
+            mstore(
+                add(p, 0x60),
+                0x52617265536b696c6c7300000000000000000000000000000000000000000000
+            )
+            mstore(0x40, add(p, 0x80))
+            return(p, 0x80)
             // return the tuple of (uint256 and string): (420, "RareSkills")
         }
     }
