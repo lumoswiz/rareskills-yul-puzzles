8c5b52df687ff2c1f93ce5aa10f8c54f9608981a
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-05 10:04:36 +0200

    feat: solve PaymentSplitter (#64)

--- src/PaymentSplitter.sol
+++ src/PaymentSplitter.sol
@@ -2,13 +2,21 @@
 pragma solidity ^0.8.13;
 
 contract PaymentSplitter {
-
     function main(address[] calldata recipients) external payable {
         assembly {
-            // your code here
+            let off := calldataload(0x04)
+            let ptr := add(0x04, off)
+            let len := calldataload(ptr)
+
+            let v := div(selfbalance(), len)
+            // prettier-ignore
+            for { let i := 0 } lt(i, len) { i := add(i, 1) } {
+                let recipient := calldataload(add(add(ptr, 0x20), shl(5, i)))
+                let r := call(gas(), recipient, v, 0x00, 0x00, 0x00, 0x00)
+            }
+
             // send the entire contract balance to the recipients
             // each recipient gets balance / recipients.length
-
         }
     }
 }
