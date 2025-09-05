d0654e30e2b52425340993e0caa2cd7af7ffb258
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-05 10:06:27 +0200

    feat: solve SendBack (#65)

--- src/SendBack.sol
+++ src/SendBack.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract SendBack {
-
     fallback() external payable {
         assembly {
-            // your code here
+            let r := call(gas(), caller(), callvalue(), 0x00, 0x00, 0x00, 0x00)
             // whatever amount of ether is sent to the contract, send it back to the sender
             // hint: use callvalue() to get the amount of ether sent to the contract
         }
