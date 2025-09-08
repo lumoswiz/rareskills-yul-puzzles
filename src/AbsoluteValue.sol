49252e7b60bc103edbb427100469e6df5811134d
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-08 12:13:15 +0200

    feat: solve AbsoluteValue (#79)

--- src/AbsoluteValue.sol
+++ src/AbsoluteValue.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-contract AbsoluteValue{
-
-  function main(int256 x) external pure returns (uint256) {
-      assembly {
-          // your code here
-          // return the absolute value of x
-          // hint: use signed comparisons
-          // hint: https://www.rareskills.io/post/signed-int-solidity
-
-      }
-  }
+contract AbsoluteValue {
+    function main(int256 x) external pure returns (uint256) {
+        assembly {
+            let n := slt(x, 0)
+            let a := add(mul(x, iszero(n)), mul(sub(0, x), n))
+            mstore(0x00, a)
+            return(0x00, 0x20)
+            // return the absolute value of x
+            // hint: use signed comparisons
+            // hint: https://www.rareskills.io/post/signed-int-solidity
+        }
+    }
 }
