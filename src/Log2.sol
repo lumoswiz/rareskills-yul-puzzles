43b88fea5d4d7cacc15d46c4a28f4e2ec139b76b
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-09-08 13:52:37 +0200

    feat: solve Log2 (#81)

--- src/Log2.sol
+++ src/Log2.sol
@@ -1,22 +1,59 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+/**
+ * Initial implementation: linear-scan over bit width
+ * Checked solady, which inspired this current impl. Ref:
+ * https://github.com/Vectorized/solady/blob/main/src/utils/FixedPointMathLib.sol#L921
+ */
+
 contract Log2 {
-    function main(uint256 x) external pure returns (uint256) {
+    function main(uint256 x) external pure returns (uint256 r) {
         assembly {
-            // your code here
-            // return log2 of x rounded down
-            // revert if x is 0
-            // hint: log2 is the index of the highest bit that is set in x
-            // for example:
-            //   bin(1) = 0001, so log2(2) = 0
-            //   bin(2) = 0010, so log2(2) = 1
-            //   bin(3) = 0011, so log2(3) = 1
-            //   bin(4) = 0100, so log2(4) = 2
-            //   bin(5) = 0101, so log2(5) = 2
-            //   bin(6) = 0110, so log2(6) = 2
-            //   bin(7) = 0111, so log2(6) = 2
-            //   bin(8) = 1000, so log2(6) = 3
+            if iszero(x) {
+                revert(0, 0)
+            }
+
+            let y := x
+
+            if gt(y, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
+                y := shr(128, y)
+                r := add(r, 128)
+            }
+
+            if gt(y, 0xFFFFFFFFFFFFFFFF) {
+                y := shr(64, y)
+                r := add(r, 64)
+            }
+
+            if gt(y, 0xFFFFFFFF) {
+                y := shr(32, y)
+                r := add(r, 32)
+            }
+
+            if gt(y, 0xFFFF) {
+                y := shr(16, y)
+                r := add(r, 16)
+            }
+
+            if gt(y, 0xFF) {
+                y := shr(8, y)
+                r := add(r, 8)
+            }
+
+            if gt(y, 0xF) {
+                y := shr(4, y)
+                r := add(r, 4)
+            }
+
+            if gt(y, 0x3) {
+                y := shr(2, y)
+                r := add(r, 2)
+            }
+
+            if gt(y, 0x1) {
+                r := add(r, 1)
+            }
         }
     }
 }
